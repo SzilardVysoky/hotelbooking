5b9b6f74d87d82edb7ec1a53676b8f11d04d89e5
SzilardVysoky <szilard.vysoky@kosickaakademia.sk>
2025-06-05 15:00:17 +0200

    add BookingCreateDTO so that POST /api/bookings shows roomId/customerId in Swagger

---
+++ src/main/java/sk/kosickaakademia/hibernatev2/dto/BookingCreateDTO.java
@@ -0,0 +1,45 @@
+package sk.kosickaakademia.hibernatev2.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class BookingCreateDTO {
+
+    @Schema(
+            description  = "ID of the Room to be booked",
+            example      = "3",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    @NotNull
+    private Integer roomId;
+
+    @Schema(
+            description  = "ID of the Customer making this booking",
+            example      = "7",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    @NotNull
+    private Integer customerId;
+
+    @Schema(
+            description  = "Start date of the booking",
+            example      = "2025-06-04",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    @NotNull
+    private LocalDate bookingDate;
+
+    @Schema(
+            description  = "End date of the booking",
+            example      = "2025-06-08",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    @NotNull
+    private LocalDate endDate;
+}
