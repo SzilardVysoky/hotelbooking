d3ac45d6eb1302f0ce852e14c72949a457af603f
SzilardVysoky <szilard.vysoky@kosickaakademia.sk>
2025-06-05 14:39:37 +0200

    add CustomerCreateDTO so that POST /api/customers shows name/email in Swagger

---
+++ src/main/java/sk/kosickaakademia/hibernatev2/dto/CustomerCreateDTO.java
@@ -0,0 +1,27 @@
+package sk.kosickaakademia.hibernatev2.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerCreateDTO {
+
+    @Schema(
+            description  = "Customer’s full name",
+            example      = "Szilard Vysoky",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    @NotBlank
+    private String name;
+
+    @Schema(
+            description  = "Customer’s email address",
+            example      = "szilard.vysoky@gmail.com",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    @NotBlank
+    private String email;
+}
